47707f1f28d27edff88acc7b3eb4ea98b244eeb4
Abdur Rahman <119144605+AbdurRahmanSikder@users.noreply.github.com>
2025-06-23 14:40:27 +0600

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <div className="details">
         <div className="full-name">Abdur Rahman Sikder Nayan</div>
         <div className="my-title">Full-Stack MERN Developer</div>
-        <div className="article">Hi, I’m a full-stack web developer specializing in the MERN stack (MongoDB, Express, React, Node.js) with a strong foundation in problem-solving and algorithmic thinking.</div>
+        <div className="article">Hi, I’m a full-stack web developer specializing in the MERN stack with a strong foundation in problem-solving and algorithmic thinking.</div>
       </div>
       <div className="codingimage">
         <img src={codingImage} alt="Coding" />
